8ef3bfa79bb3d32f0647fe92e0ef54eaaf0e7db7
chrisDelf <chrisdel@protonmail.com>
2023-12-28 15:48:54 -0500

    handling tilemaps

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -4,8 +4,8 @@ var can_laser: bool = true
 var can_grenade: bool = true
 
 #custom signals
-signal laser
-signal grenade
+signal laser(position, direction)
+signal grenade(position, direction)
 
 func _process(_delta):
 	
@@ -15,23 +15,30 @@ func _process(_delta):
 	#move and slide uses slider already
 	move_and_slide()
 	
+	# rotate
+	look_at(get_global_mouse_position())
+	var player_direction = (get_global_mouse_position() - position).normalized()
 	# laser shooting input
 	if Input.is_action_pressed("primary action") and can_laser:
+		$LaserCooldown.start()
 		#randomly selected a marker 2d for the laser start
 		var laser_markers = $LaserStartPositions.get_children()
 		#randi() followed by % then the max size of the list to create a simple randomizer
 		var selected_laser = laser_markers[randi() % laser_markers.size()]
+		
 		can_laser = false
-		$LaserCooldown.start()
+		
 		# emit the position when selected
-		laser.emit(selected_laser.global_position)
+		laser.emit(selected_laser.global_position, player_direction)
 		
 	
 	# Grenade Tossing input
 	if Input.is_action_just_pressed("secondary action") and can_grenade:
-		can_grenade = false
 		$GrenadeTimer.start()
-		grenade.emit()
+		#for now I'm going to use the laser markers
+		var pos_marker = $LaserStartPositions.get_children()[0]
+		can_grenade = false
+		grenade.emit(pos_marker.global_position, player_direction)
 		
 
 
