6098eb0f8ae814bf0cbd77e845c1d3ce3cde374a
chrisDelf <chrisdel@protonmail.com>
2024-01-03 19:29:46 -0500

    got through animations and tweens

--- Scenes/Levels/level.gd
+++ Scenes/Levels/level.gd
@@ -6,17 +6,17 @@ var grenade_scene: PackedScene = preload("res://Scenes/projectiles/grenade.tscn"
 
 
 
-func _on_gate_player_entered_gate(body):
-	print("Player has entered gate")
-	print(body)
+func _on_gate_player_entered_gate(_body):
+	var tween = create_tween()
+	tween.tween_property($Player, "speed",0,0.5)
 
 	
 
-func _on_player_laser(position, direction):
+func _on_player_laser(position1, direction):
 	# we are creating an instance of the node
 	var laser = laser_scene.instantiate() as Area2D
 	#update the position of the laser
-	laser.position = position
+	laser.position = position1
 	laser.rotation_degrees  = rad_to_deg(direction.angle()) + 90
 	laser.direction = direction
 	
@@ -26,10 +26,24 @@ func _on_player_laser(position, direction):
 
 
 
-func _on_player_grenade(position, direction):
+func _on_player_grenade(position1, direction):
 	var grenade = grenade_scene.instantiate() as RigidBody2D
-	grenade.position = position
+	grenade.position = position1
 	grenade.linear_velocity = direction * grenade.speed
 	$Projectiles.add_child(grenade)
 	
 	
+
+
+func _on_house_player_entered(_body):
+	var tween = get_tree().create_tween()
+	#zooms in when the character enters the house.
+	tween.set_parallel(true)
+	tween.tween_property($Player/Camera2D, "zoom", Vector2(1,1),1).set_trans(Tween.TRANS_QUAD)
+	#tween.tween_property($Player, "modulate:a",0,2).from(0.5)
+
+
+func _on_house_player_leave(_body):
+	var tween = get_tree().create_tween()
+	#zooms out when the character enters the house.
+	tween.tween_property($Player/Camera2D, "zoom", Vector2(.6,.6),1)
